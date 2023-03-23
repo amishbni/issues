1952d1a9fb4c2da713a9a7c2aee1197854b755b2
Amir Shabani <amir@shabani.dev>
2023-03-23 19:58:41 +0330

    Handle and decode response

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -3,7 +3,9 @@ defmodule Issues.CLI do
   @default_count 10
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   def parse_args(argv) do
@@ -24,4 +26,20 @@ defmodule Issues.CLI do
    :help
   end
 
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+  end
+
+  def decode_response({:ok, body}), do: body
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching from Github: #{error["message"]}"
+    System.halt(2)
+  end
 end
--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -13,11 +13,12 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({:ok, %{status_code: 200, body: body}}) do
-    {:ok, body}
+  def handle_response({_, %{status_code: status_code, body: body}}) do {
+    status_code |> check_status_code(),
+    body |> Poison.Parser.parse!()
+  }
   end
 
-  def handle_response({_, %{status_code: _, body: body}}) do
-    {:error, body}
-  end
+  defp check_status_code(200), do: :ok
+  defp check_status_code(_), do: :error
 end
